2e0ec142a3dc595d0b792bbac6c7a7f6a03d43cb
bargiro1 <76563bar@gmail.com>
2025-01-23 21:09:45 +0200

    Initialize project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './components/Board'; 
+import './styles/App.css'; 
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>X Mix Drix</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
